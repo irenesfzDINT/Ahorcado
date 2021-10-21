9f6b967d0de66b3199cac62ea8875bbfd6368739
irene <irene.sanchez@iesdoctorbalmis.com>
2021-10-21 12:54:28 +0200

    cambios en codigo

--- Ahorcado/MainWindow.xaml.cs
+++ Ahorcado/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Ahorcado
     public partial class MainWindow : Window
     {
         Char[] abecedario = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-        String[] palabras = { "hola", "adios", "DINT", "sdfsdfasdfasdfsad" };
+        String[] palabras = { "hola", "adios", "DINT", "sdfsdfasdfasdfsad", "yucca", "PSP", "Apruebame" };
         String palabra = null;
         Char[] letrasPalabra = null;
         Random rn = new Random();
@@ -39,12 +39,14 @@ namespace Ahorcado
             {
                 Button button = new Button();
                 button.Content = letra.ToString().ToUpper();
-                button.Tag = letra.ToString().ToUpper();
+                button.Tag = letra.ToString();
                 button.Click += Button_Click;
-                button.KeyUp += Button_KeyUp;
+
                 CuadriculaUniformGrid.Children.Add(button);
             }
         }
+
+
         private void CreaGuiones(String palabra)
         {
             letrasPalabra = palabra.ToCharArray();
@@ -52,7 +54,6 @@ namespace Ahorcado
             for (int i = 0; i < numeroLetras; i++)
             {
                 TextBlock texto = new TextBlock();
-                //texto.Name = "textoTextBlock";
                 texto.Text = "_";
                 texto.FontSize = 100;
                 texto.Tag = letrasPalabra[i].ToString();
@@ -65,38 +66,58 @@ namespace Ahorcado
             palabra = palabras[rn.Next(0, palabras.Length)];
             return palabra;
         }
-        private void Button_KeyUp(object sender, KeyEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
 
         public void CompruebaCoincidencia(String letra)
         {
             letrasPalabra = palabra.ToCharArray();
-
             if (palabra.Contains(letra))
             {
+                foreach (TextBlock tb in PalabraWrapPanel.Children)
+                {
+                    if (tb.Tag.ToString() == letra) tb.Text = letra.ToUpper();
+                }
             }
             else
             {
-                
                 intentos--;
+                if (intentos == 0)
+                {
+                    TodoMal();
+                }
             }
         }
+        private void TodoMal()
+        {
+            foreach (Button boton in CuadriculaUniformGrid.Children)
+            {
+                boton.IsEnabled = false;
+            }
+            MessageBox.Show("Has perdido");
+            int contador = 0;
+            //muestra palabra
+            letrasPalabra = palabra.ToCharArray();
+            foreach (TextBlock tb in PalabraWrapPanel.Children)
+            {
+                tb.Text = letrasPalabra[contador].ToString().ToUpper();
+                contador++;
+            }
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            String letra  = (sender as Button).Tag.ToString();
-            letrasPalabra = palabra.ToCharArray();
+            String letra = (sender as Button).Tag.ToString();
+            (sender as Button).IsEnabled = false;
+            CompruebaCoincidencia(letra);
+        }
 
-            if (palabra.Contains(letra))
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            foreach (Button boton in CuadriculaUniformGrid.Children)
             {
-
+                if (e.Key.ToString().Contains(boton.Tag.ToString().ToUpper()))
+                    boton.IsEnabled = false;
             }
-            else
-            {
-
-                intentos--;
-            }
+            CompruebaCoincidencia(e.Key.ToString().ToLower());
         }
     }
 }
